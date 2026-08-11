6065ecbc4282f51b50a8b34862be286592d74be3
jawc-05 <joaoalfredowc@gmail.com>
2026-08-11 15:29:17 -0300

    feat: adding service pkg in the order-service with the basics methods

---
+++ delivery-logistics-backend/services/order-service/src/main/java/br/com/jawc/logistics/order_service/service/OrderService.java
@@ -0,0 +1,30 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.logistics.order_service.service;
+
+import br.com.jawc.logistics.order_service.domain.Order;
+import br.com.jawc.logistics.order_service.repository.IOrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+    private final IOrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(IOrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Order createOrder(Order order) {
+        //Here i will need to update with some "filter" and so on
+        return orderRepository.save(order);
+    }
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+}
