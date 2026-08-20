da4ef1d00f437dd7e1bf26ffeacb971fb6faf3ae
jawc-05 <joaoalfredowc@gmail.com>
2026-08-20 13:16:47 -0300

    feat: added UserDetails in the USer domain in the auth-service

--- delivery-logistics-backend/services/auth-service/src/main/java/br/com/jawc/logistics/auth_service/domain/User.java
+++ delivery-logistics-backend/services/auth-service/src/main/java/br/com/jawc/logistics/auth_service/domain/User.java
@@ -8,8 +8,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -17,7 +22,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -37,5 +42,41 @@ public class User {
     @PrePersist
     protected void onCreate() {
         this.createdAt = LocalDateTime.now();
+    }
+
+    // =========================================================
+    // MÉTODOS OBRIGATÓRIOS DA INTERFACE USERDETAILS
+    // =========================================================
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        //CONVERTE O CAMPO role PARA O PADRAO SPRING SECURITY
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.role));
+    }
+
+    @Override
+    public String getUsername(){
+        //campo "username" é o email em login
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
