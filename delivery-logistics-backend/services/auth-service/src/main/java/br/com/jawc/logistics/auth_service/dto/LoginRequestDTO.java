4b07f65e26ce4d049217f4a0483ff7b58b66ad61
jawc-05 <joaoalfredowc@gmail.com>
2026-08-20 12:20:37 -0300

    feat: added dto pkg and LoginRequestDTO record in the dto pkg

---
+++ delivery-logistics-backend/services/auth-service/src/main/java/br/com/jawc/logistics/auth_service/dto/LoginRequestDTO.java
@@ -0,0 +1,13 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.logistics.auth_service.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequestDTO(
+        @NotBlank @Email String email,
+        @NotBlank String password
+) {
+}
