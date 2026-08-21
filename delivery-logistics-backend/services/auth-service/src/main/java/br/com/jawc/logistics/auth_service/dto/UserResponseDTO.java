8f60bbddb2298a611c8d8c75eeec0bb89098db09
jawc-05 <joaoalfredowc@gmail.com>
2026-08-21 15:45:50 -0300

    feat: updated UserREsponseDTO to have the ID

--- delivery-logistics-backend/services/auth-service/src/main/java/br/com/jawc/logistics/auth_service/dto/UserResponseDTO.java
+++ delivery-logistics-backend/services/auth-service/src/main/java/br/com/jawc/logistics/auth_service/dto/UserResponseDTO.java
@@ -9,6 +9,7 @@ import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 public record UserResponseDTO(
+        Long id,
         String email,
         String role,
         LocalDateTime createdAt
