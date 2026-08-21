54fd742d75495c087ee1263223ea74d369525684
jawc-05 <joaoalfredowc@gmail.com>
2026-08-21 14:03:10 -0300

    feat: updated TOkenService in auth-service to validateTOken

--- delivery-logistics-backend/services/auth-service/src/main/java/br/com/jawc/logistics/auth_service/service/TokenService.java
+++ delivery-logistics-backend/services/auth-service/src/main/java/br/com/jawc/logistics/auth_service/service/TokenService.java
@@ -7,6 +7,7 @@ import br.com.jawc.logistics.auth_service.domain.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -32,6 +33,19 @@ public class TokenService {
                     .sign(algorithm);
         }catch (JWTCreationException e){
             throw new RuntimeException("Erro ao gerar JWT", e);
+        }
+    }
+
+    public String validateToken(String token) {
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth-service")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException e){
+            throw new RuntimeException("Erro ao validar JWT", e);
         }
     }
 }
