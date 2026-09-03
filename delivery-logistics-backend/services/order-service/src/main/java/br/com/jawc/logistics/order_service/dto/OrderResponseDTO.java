9a9b8e6acaaa7753ba4a08af2383ccd5cbef416b
jawc-05 <joaoalfredowc@gmail.com>
2026-09-03 15:42:47 -0300

    feat: add courierId in the OrderResponseDTO

--- delivery-logistics-backend/services/order-service/src/main/java/br/com/jawc/logistics/order_service/dto/OrderResponseDTO.java
+++ delivery-logistics-backend/services/order-service/src/main/java/br/com/jawc/logistics/order_service/dto/OrderResponseDTO.java
@@ -14,6 +14,7 @@ public record OrderResponseDTO(
         String recipientName,
         BigDecimal totalAmount,
         OrderStatus status,
-        LocalDateTime createdAt
+        LocalDateTime createdAt,
+        Long courierId
 ) {
 }
