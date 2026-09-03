7d5404f9edb76d299012f9d5cc044c41515a2330
jawc-05 <joaoalfredowc@gmail.com>
2026-09-03 15:33:16 -0300

    feat: added DeliveryClient interface and its methods

--- delivery-logistics-backend/services/order-service/src/main/java/br/com/jawc/logistics/order_service/service/OrderService.java
+++ delivery-logistics-backend/services/order-service/src/main/java/br/com/jawc/logistics/order_service/service/OrderService.java
@@ -4,11 +4,12 @@
 package br.com.jawc.logistics.order_service.service;
 
 import br.com.jawc.logistics.order_service.domain.Order;
+import br.com.jawc.logistics.order_service.dto.CourierResponseDTO;
 import br.com.jawc.logistics.order_service.dto.OrdersPerDayDTO;
+import br.com.jawc.logistics.order_service.feign.DeliveryClient;
 import br.com.jawc.logistics.order_service.repository.IOrderRepository;
 import br.com.jawc.logistics.order_service.repository.OrderReportRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -20,9 +21,12 @@ import java.util.List;
 public class OrderService {
     private final IOrderRepository orderRepository;
     private final OrderReportRepository orderReportRepository;
+    private final DeliveryClient deliveryClient;
 
     public Order createOrder(Order order) {
-        //Here i will need to update with some "filter" and so on
+        CourierResponseDTO courier = deliveryClient.getAvailableCourier();
+
+        order.setCourierId(courier.id());
         return orderRepository.save(order);
     }
 
