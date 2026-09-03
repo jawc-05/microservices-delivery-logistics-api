54cd093f501da5a8b2351dec091a847252e9f379
jawc-05 <joaoalfredowc@gmail.com>
2026-09-03 00:37:24 -0300

    feat: added POST endpoint in CourierController

--- delivery-logistics-backend/services/delivery-service/src/main/java/br/com/jawc/logistics/delivery_service/controller/CourierController.java
+++ delivery-logistics-backend/services/delivery-service/src/main/java/br/com/jawc/logistics/delivery_service/controller/CourierController.java
@@ -30,4 +30,31 @@ public class CourierController {
     private final CourierService courierService;
 
 
+    @PostMapping
+    @Operation(summary = "Create a COurier")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "The courier was created"),
+            @ApiResponse(responseCode = "400", description = "Validation error or duplicate key")
+    })
+    public ResponseEntity<CourierResponseDTO> createCourier(@RequestBody @Valid CourierRequestDTO request){
+        //TRANSFORMANDO DTO DE ENTRADA NA ENTIDADE QUE VAI AO BANCO
+
+        Courier newCourier = new Courier();
+        newCourier.setDocument(request.document());
+        newCourier.setName(request.name());
+        newCourier.setEmail(request.email());
+        newCourier.setPhone(request.phone());
+
+        //SALVA NO DB
+        Courier courierCreated = courierService.createCourier(newCourier);
+
+        //TRANSFORMO A ENTITY SALVA NO DTO DE SAIDA
+        var dto = new CourierResponseDTO(
+                courierCreated.getId(),
+                courierCreated.getName(),
+                courierCreated.getPhone(),
+                courierCreated.getStatus()
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(dto);
+    }
 }
