ebc6e5cf6d428d6035b5d927d916b2aae57945b8
jawc-05 <joaoalfredowc@gmail.com>
2026-09-03 00:40:06 -0300

    feat: aded getAllCOurier method in CourierService

--- delivery-logistics-backend/services/delivery-service/src/main/java/br/com/jawc/logistics/delivery_service/service/CourierService.java
+++ delivery-logistics-backend/services/delivery-service/src/main/java/br/com/jawc/logistics/delivery_service/service/CourierService.java
@@ -6,6 +6,8 @@ package br.com.jawc.logistics.delivery_service.service;
 import br.com.jawc.logistics.delivery_service.domain.Courier;
 import br.com.jawc.logistics.delivery_service.repository.ICourierRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,5 +17,9 @@ public class CourierService {
 
     public Courier createCourier(Courier courier){
         return courierRepository.save(courier);
+    }
+
+    public Page<Courier> getAllCouriers(Pageable pageable){
+        return courierRepository.findAll(pageable);
     }
 }
