8ce4554fc076917155cb99c70c139ca6e1bc38d3
jawc-05 <joaoalfredowc@gmail.com>
2026-09-03 15:20:58 -0300

    feat: updated DeliveryClient using CourierResponseDTO

--- delivery-logistics-backend/services/order-service/src/main/java/br/com/jawc/logistics/order_service/feign/DeliveryClient.java
+++ delivery-logistics-backend/services/order-service/src/main/java/br/com/jawc/logistics/order_service/feign/DeliveryClient.java
@@ -3,6 +3,7 @@
  */
 package br.com.jawc.logistics.order_service.feign;
 
+import br.com.jawc.logistics.order_service.dto.CourierResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,5 +11,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 public interface DeliveryClient {
 
     @GetMapping("/available")
-    Object getAvailableCourier();
+    CourierResponseDTO getAvailableCourier();
 }
