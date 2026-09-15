d6ff2b417ed29390f961f3272bd420125e5555a0
jawc-05 <joaoalfredowc@gmail.com>
2026-09-15 15:45:45 -0300

    fix: change the name of the method(findByOrderId), to now work properly

--- delivery-logistics-backend/services/notification-service/src/main/java/br/com/jawc/logistics/notification_service/repository/NotificationRepository.java
+++ delivery-logistics-backend/services/notification-service/src/main/java/br/com/jawc/logistics/notification_service/repository/NotificationRepository.java
@@ -4,6 +4,8 @@
 package br.com.jawc.logistics.notification_service.repository;
 
 import br.com.jawc.logistics.notification_service.domain.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +14,5 @@ import java.util.List;
 @Repository
 public interface NotificationRepository extends MongoRepository<Notification, String> {
 
-    List<Notification> findOrderById(Long orderId);
+    Page<Notification> findByOrderId(Long orderId, Pageable pageable);
 }
