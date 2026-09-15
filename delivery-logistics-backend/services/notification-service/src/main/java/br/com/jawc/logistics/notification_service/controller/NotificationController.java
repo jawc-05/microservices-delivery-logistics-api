bf11541fa3d33821224dabd1f3817446f8506aae
jawc-05 <joaoalfredowc@gmail.com>
2026-09-15 15:44:48 -0300

    feat: add GET endpoint in Notification COntroller

--- delivery-logistics-backend/services/notification-service/src/main/java/br/com/jawc/logistics/notification_service/controller/NotificationController.java
+++ delivery-logistics-backend/services/notification-service/src/main/java/br/com/jawc/logistics/notification_service/controller/NotificationController.java
@@ -8,16 +8,21 @@ import br.com.jawc.logistics.notification_service.dto.NotificationRequestDTO;
 import br.com.jawc.logistics.notification_service.dto.NotificationResponseDTO;
 import br.com.jawc.logistics.notification_service.service.NotificationService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/notifications")
@@ -25,6 +30,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class NotificationController {
 
     private final NotificationService notificationService;
+
+    @GetMapping("/order/{orderId}")
+    @Operation(summary = "Search the notification by order id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "returns the notification by id"),
+            @ApiResponse(responseCode = "404", description = "This ORDER doesnt have an notification"),
+            @ApiResponse(responseCode = "400", description = "syntax error or bad request",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, examples = @ExampleObject(value = "BAD_REQUEST"))),
+            @ApiResponse(responseCode = "500", description = "An exception was made",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, examples = @ExampleObject(value = "INTERNAL_SERVER_ERROR"))),
+    })
+    public ResponseEntity<Page<NotificationResponseDTO>> searchNotifications(
+            @PathVariable Long orderId,
+            @PageableDefault(size = 10, sort = "createdAt") Pageable pageable) {
+
+        //ACHANDO NOTIFICATION
+        Page<Notification> notifications = notificationService.getNotificationsByOrderId(orderId, pageable);
+
+        //CRIANDO DTO
+        Page<NotificationResponseDTO> responseDTO = notifications.map(notification -> new NotificationResponseDTO(
+                notification.getId(),
+                notification.getOrderId(),
+                notification.getMessage(),
+                notification.getCreatedAt()
+        ));
+        return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
+    }
 
     @PostMapping
     @Operation(summary = "Create a Notification")
