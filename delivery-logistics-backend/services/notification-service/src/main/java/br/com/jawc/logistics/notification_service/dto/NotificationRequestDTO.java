362892df06f6a8098af66c4a0531c77bcc508b6f
jawc-05 <joaoalfredowc@gmail.com>
2026-09-15 14:32:48 -0300

    feat: add DTO pkg and NotificationRequestDTO

---
+++ delivery-logistics-backend/services/notification-service/src/main/java/br/com/jawc/logistics/notification_service/dto/NotificationRequestDTO.java
@@ -0,0 +1,17 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.logistics.notification_service.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record NotificationRequestDTO(
+        @NotNull(message = "Order ID cannot be NULL")
+        Long orderId,
+
+        @NotBlank(message = "Message cannot be BLANK")
+        String message
+
+) {
+}
