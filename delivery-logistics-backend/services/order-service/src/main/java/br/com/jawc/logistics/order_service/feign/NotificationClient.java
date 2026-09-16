da48a1b735ff75bf786c630515e3cbd6ba7ef377
jawc-05 <joaoalfredowc@gmail.com>
2026-09-16 13:59:05 -0300

    fix: add the correct import in the NotificationClient

--- delivery-logistics-backend/services/order-service/src/main/java/br/com/jawc/logistics/order_service/feign/NotificationClient.java
+++ delivery-logistics-backend/services/order-service/src/main/java/br/com/jawc/logistics/order_service/feign/NotificationClient.java
@@ -3,6 +3,7 @@
  */
 package br.com.jawc.logistics.order_service.feign;
 
+import br.com.jawc.logistics.order_service.dto.NotificationRequestDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
